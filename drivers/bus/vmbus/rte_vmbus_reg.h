ae58a4565871fa91803d61078a36bf6aae08c7b2
Long Li <longli@microsoft.com>
2025-04-10 22:41:53 +0000

    staged

--- drivers/bus/vmbus/rte_vmbus_reg.h
+++ drivers/bus/vmbus/rte_vmbus_reg.h
@@ -100,16 +100,11 @@ struct __rte_packed_begin vmbus_bufring {
 		uint32_t value;
 	} feature_bits;
 
-	/* Pad it to rte_mem_page_size() so that data starts on page boundary */
-//	uint8_t	reserved2[4028];
-	uint8_t reserved2[65468];
-//	uint8_t reserved2[rte_mem_page_size() - 68];
-
 	/*
-	 * Ring data starts here + RingDataStartOffset
-	 * !!! DO NOT place any fields below this !!!
+	 * This the end of ring buffer head. The ring buffer data is system
+	 * page aligned and starts at rte_mem_page_size() from the beginning
+	 * of this structure
 	 */
-	uint8_t data[];
 } __rte_packed_end;
 
 /*
