0f98caa036690b15ae9e8dc7cb7bc100f3f90fd6
Long Li <longli@microsoft.com>
2026-01-08 20:59:47 +0000

    net/netvsc: Dealloc PDs on device close

--- drivers/net/mana/mana.c
+++ drivers/net/mana/mana.c
@@ -282,6 +282,16 @@ mana_dev_close(struct rte_eth_dev *dev)
 	if (ret)
 		return ret;
 
+	if (priv->ib_parent_pd) {
+		ibv_dealloc_pd(priv->ib_parent_pd);
+		priv->ib_parent_pd = NULL;
+	}
+
+	if (priv->ib_pd) {
+		ibv_dealloc_pd(priv->ib_pd);
+		priv->ib_pd = NULL;
+	}
+
 	ret = ibv_close_device(priv->ib_ctx);
 	if (ret) {
 		ret = errno;
